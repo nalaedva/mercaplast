c9d665e7831d684cb5ac67e0cc1d306fca628752
Valeria <valecvelazquez@gmail.com>
2022-08-26 10:32:22 +0200

    Ejemplo useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import NavBar from './components/General/NavBar/NavBar'
 import Chips from './components/General/Products/Chips'
 import FeaturedProducts from './components/General/Products/FeaturedProducts'
@@ -5,11 +6,21 @@ import FeaturedProducts from './components/General/Products/FeaturedProducts'
 
 
 function App() {
+    const [nombre, setNombre ] = useState ('');
+
+    const actualizarNombre = (e) => {
+        setNombre(e.target.value);
+    }
+
     return ( 
         <>
         <NavBar />
         <Chips />
         <FeaturedProducts />
+        <p>{nombre}</p>
+        {/* <button onClick={() => setNombre('Valeria')}>Cambiar nombre</button> */}
+
+        {/* <input type="text" value={nombre} onChange={actualizarNombre} /> */}
         </>
         
     );
